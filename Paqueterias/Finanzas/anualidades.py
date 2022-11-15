f88fa7b6bd4588016d2429429d866a57a064673f
DavidHC11 <67530511+DavidHC11@users.noreply.github.com>
2022-11-14 19:41:32 -0600

    Se agrega función suma y resta

--- Paqueterias/Finanzas/anualidades.py
+++ Paqueterias/Finanzas/anualidades.py
@@ -1,6 +1,14 @@
-def resta(lista):
+def suma(lista):
     suma = 0
     for x in lista:
-        suma -= x
+        suma += x
 
     return suma
+
+
+def resta(lista):
+    resta = 0
+    for x in lista:
+        resta -= x
+
+    return resta 
